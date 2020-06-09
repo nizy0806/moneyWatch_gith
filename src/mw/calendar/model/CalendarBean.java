5d945afabe68e479538c4198a5185c2982cb8eda
Maro <stand.atop@gmail.com>
2020-06-09 15:23:40 +0900

    MwScheduleDTO

--- src/mw/calendar/model/CalendarBean.java
+++ src/mw/calendar/model/CalendarBean.java
@@ -1,10 +1,14 @@
 package mw.calendar.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CalendarBean {
+	
+	@Autowired
+	private MwScheduleDTO mwscheduleDTO = null;
 
 	@RequestMapping("Calendar.mw")
 	public String cal() {
